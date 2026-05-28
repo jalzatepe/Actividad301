1a9295da17c31375d083f2353c3fc83b1254e0ee
Usuario <Usuario@JUAN>
2026-05-28 16:13:39 -0500

    Ejerciciopropuesto802

---
+++ src/main/java/com/mycompany/ejerciciopropuesto803/Ejerciciopropuesto803.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopropuesto803;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejerciciopropuesto803 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal =new VentanaPrincipal ();;
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
